befe99491875ca16cfe2bbbd95f8d38cd87e3d27
Ikechukwu Chiemelie Charles <chiemeliecharleschristopher@gmail.com>
2024-07-17 18:33:58 -0700

    added readme

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,7 @@ import Form from "./Form";
 
 function App() {
   return (
-    <div className="bg-Greenlighter w-full h-full flex  justify-center pt-20 flex-col gap-5">
-      {/* <h1 className="text-3xl font-bold underline text-Greenmedium font-Karla">
-        Hello world!
-      </h1> */}
+    <div className="bg-Greenlighter w-full flex  justify-center pt-20 flex-col gap-5">
       <Form />
       <Attribution />
     </div>
